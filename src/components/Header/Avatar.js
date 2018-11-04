7344af9d6958a85dd2ad75064cf66beeb00344c0
Tom Conder <tomc@gazpacho.net>
2018-11-04 11:12:04 -0600

    Add link to avatar

--- src/components/Header/Avatar.js
+++ src/components/Header/Avatar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import styles from './Avatar.module.scss'
 import ItsTomIcon from '../Icons/ItsTomIcon'
 
 const Avatar = () => (
   <div className={styles.title}>
-    <ItsTomIcon size={32} aria-label="It's Tom" />
+    <Link to="/">
+      <ItsTomIcon size={32} aria-label="It's Tom" />
+    </Link>
   </div>
 )
 
