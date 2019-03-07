8d4c8e89fa456705e7ce7b94b3a7c662f11e8101
tomconder <tomc@gazpacho.net>
2019-03-07 17:03:46 -0500

    add ItsTomMug icon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,6 +1,7 @@
 import BrainJarIcon from './BrainJarIcon'
 import GithubIcon from './GithubIcon'
 import ItsTomIcon from './ItsTomIcon'
+import ItsTomMugIcon from './ItsTomMugIcon'
 import MeepleGroupIcon from './MeepleGroupIcon'
 import MeepleIcon from './MeepleIcon'
 import MugIcon from './MugIcon'
@@ -10,6 +11,7 @@ export {
   BrainJarIcon,
   GithubIcon,
   ItsTomIcon,
+  ItsTomMugIcon,
   MeepleGroupIcon,
   MeepleIcon,
   MugIcon,
