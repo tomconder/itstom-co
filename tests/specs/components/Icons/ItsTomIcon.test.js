ee1e60df83ab90eb2a9e59248dcc553237ad7186
Tom Conder <tomc+github@gazpacho.net>
2019-07-05 15:19:03 -0500

    Add a11y tests

--- tests/specs/components/Icons/ItsTomIcon.test.js
+++ tests/specs/components/Icons/ItsTomIcon.test.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Chance from 'chance'
-import { shallow } from 'enzyme'
+import { render, shallow } from 'enzyme'
+import { axe, toHaveNoViolations } from 'jest-axe'
 import { ItsTomIcon } from '../../../../src/components/Icons'
+
+expect.extend(toHaveNoViolations)
 
 describe('ItsTomIcon', () => {
   it('renders', () => {
@@ -11,5 +14,16 @@ describe('ItsTomIcon', () => {
     )
     expect(wrapper.find('svg').props().height).toEqual(size)
     expect(wrapper.find('svg').props().width).toEqual(size)
+  })
+
+  it('a11y', async () => {
+    const size = Chance().natural({ max: 256 })
+    const wrapper = render(
+      <ItsTomIcon size={size} />,
+    )
+
+    const result = await axe(wrapper.html())
+
+    expect(result).toHaveNoViolations()
   })
 })
