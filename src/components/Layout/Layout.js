0f679f47471c7bc0b77b88c5b7d9b5bd2603ed0f
Tom Conder <tomc+github@gazpacho.net>
2019-10-29 23:59:51 -0500

    use react-head to eliminate use of react-side-effect

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { createGlobalStyle } from 'styled-components'
-import Helmet from 'react-helmet'
+import {
+  HeadProvider, Title, Link, Meta,
+} from 'react-head'
 import { Header } from '../Header'
 import Footer from '../Footer'
 import styles from './Layout.module.scss'
@@ -23,17 +25,15 @@ const Layout = ({ children }) => {
   return (
     <div className={styles.base}>
       <GlobalStyle />
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta
+      <HeadProvider>
+        <Meta charSet="utf-8" />
+        <Meta
           name="description"
           content="The personal web page of Tom Conder"
         />
-        <html lang="en" />
-        <title lang="en">{title}</title>
-        <link rel="canonical" href="https://itstom.co/" />
-      </Helmet>
-
+        <Title lang="en">{title}</Title>
+        <Link rel="canonical" href="https://itstom.co/" />
+      </HeadProvider>
       <Header />
       {children}
       <Footer />
