7c0a3901eb74af4355dfcada98f5688fa8faee5a
Tom Conder <tomc+github@gazpacho.net>
2019-12-02 22:51:55 -0600

    fix stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,14 +1,12 @@
-/* eslint-disable max-len,import/no-dynamic-require,global-require */
-module.exports = function (config) {
-  config.set({
-    mutate: ['src/**/*.js?(x)', '!src/**/*@(.test|.spec|Spec).js?(x)'],
-    mutator: 'javascript',
-    testRunner: 'jest',
-    reporters: ['progress', 'clear-text', 'html'],
-    coverageAnalysis: 'off',
-    jest: {
-      projectType: 'react',
-      config: require(`${ __dirname }/jest.config.js`),
-    },
-  })
+/* eslint-disable import/no-dynamic-require,global-require */
+module.exports = {
+  mutate: ['src/**/*.js?(x)', '!src/**/*@(.test|.spec|Spec).js?(x)'],
+  mutator: 'javascript',
+  testRunner: 'jest',
+  reporters: ['progress', 'clear-text', 'html'],
+  coverageAnalysis: 'off',
+  jest: {
+    projectType: 'react',
+    config: require(`${ __dirname }/jest.config.js`),
+  },
 }
