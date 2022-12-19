155cdfdd4882f5aa21e2e160707c46df224250b7
Tom Conder <tom@itstom.co>
2022-12-18 23:29:17 -0600

    Upgrade gatsby (#36)
    * build :: update jsdom
    
    * build :: upgrade to gatsby 5
    
    * build :: update yarn.lock
    
    * build :: use node 18
    
    * build :: use jammy dist

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,5 +1,10 @@
 import Enzyme from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
+import util from 'util'
+
+Object.defineProperty(global, 'TextEncoder', {
+  value: util.TextEncoder,
+})
 
 Enzyme.configure({
   adapter: new Adapter(),
